d390cc9c1fd490a4d1d95dbaadd71ed272b4645a
Katrina Tollin <ktollin@gmail.com>
2017-03-05 12:03:36 -0600

    update omniauth references

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,40 @@ class User < ApplicationRecord
 
   has_many :articles
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+    end
+  end
+
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new session["devise.user_attributes"] do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
+
+  # def password_required?
+  #   super && provider.blank?
+  # end
+
+  # def update_with_password(params, *options)
+  #   if encrypted_password.blank?
+  #     update_attributes(params, *options)
+  #   else
+  #     super
+  #   end
+  # end
+
+    # data = auth.info
+    # user = User.where(:email => data["email"]).first
 
     # Uncomment the section below if you want users to be created if they don't exist
     # unless user
@@ -18,7 +49,7 @@ class User < ApplicationRecord
     #        password: Devise.friendly_token[0,20]
     #     )
     # end
-    user
+    # user
   #     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
   #       user.provider = auth.provider
   #       user.uid = auth.uid
@@ -26,5 +57,5 @@ class User < ApplicationRecord
   #       user.password = Devise.friendly_token[0,20]
   #     end
   # end
-  end
+  # end
 end
