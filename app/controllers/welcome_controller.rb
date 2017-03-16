ea21fd1feabbaa5a85d4c0285e01f430561216c4
Katrina Tollin <ktollin@gmail.com>
2017-03-16 14:51:12 -0500

    add recent articles to welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
+    @recent_articles = Article.all.limit(2).order(:created_at)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,5 @@ class Article < ApplicationRecord
   validates :title, presence: true,
                     length: { minimum: 5 }
   belongs_to :user
+
 end
